e19d2d8eb0f84fcdae77a9960d460bf3d3575a7c
Keven Alves <kevenalvesm@gmail.com>
2021-11-01 18:39:18 -0300

    projeto sunny page finalizado

--- sunnyside-agency-landing-page/js/app.js
+++ sunnyside-agency-landing-page/js/app.js
@@ -17,3 +17,63 @@ menu.onclick = () =>{
     }
 
 }
+
+// Scroll Suave
+
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+
+    window.scroll({
+        top: to - 30,
+        behavior: "smooth",
+    });
+}
+
+// animacao
+
+// Debounce do Lodash
+// const debounce = function(func, wait, immediate) {
+//     let timeout;
+//     return function(...args) {
+//       const context = this;
+//       const later = function () {
+//         timeout = null;
+//         if (!immediate) func.apply(context, args);
+//       };
+//       const callNow = immediate && !timeout;
+//       clearTimeout(timeout);
+//       timeout = setTimeout(later, wait);
+//       if (callNow) func.apply(context, args);
+//     };
+//   };
+  
+  const target = document.querySelectorAll('[data-anime]');
+  const animationClass = 'animate';
+  
+  function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+      if((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass);
+      } else {
+        element.classList.remove(animationClass);
+      }
+    })
+  }
+  
+  animeScroll();
+  
+  if(target.length) {
+      window.addEventListener('scroll', function() {
+          animeScroll();
+      })
+  }
